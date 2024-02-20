1ce16ea3705bc059ec43d2a099ced4c0be1df644
MatheusBProgrammer <matheusbarretoprogramador@gmail.com>
2024-02-20 00:45:59 -0300

    validation updates

--- src/controllers/SessionController.js
+++ src/controllers/SessionController.js
@@ -1,5 +1,6 @@
 import User from "../models/User";
 import UserValidation from "../models/validation/UserValidation";
+import bcrypt from "bcryptjs";
 
 class SessionController {
   async index(req, res) {
@@ -17,20 +18,22 @@ class SessionController {
   async store(req, res) {
     const { email, password } = req.body;
 
+    //validação yup
     try {
       await UserValidation.validate({ email, password });
     } catch (error) {
-      return res.status(400).json({ erro: erro de validação });
+      return res.status(400).json({ erro: "erro de validação" });
     }
 
     try {
       let user = await User.findOne({ email: email });
       if (!user) {
+        const hashPassword = await bcrypt.hashSync(password, 10);
         user = await User.create({
           email: email,
-          password: password,
+          password: hashPassword,
         });
-        return res.json({ user });
+        return res.json({ message: "Cadastro realizado com sucesso!" });
       }
       return res
         .status(400)
@@ -38,6 +41,29 @@ class SessionController {
     } catch (error) {
       return res.status(500).json({
         error: `Erro ao criar usuário: ${error}`,
+      });
+    }
+  }
+
+  async login(req, res) {
+    const { email, password } = req.body;
+
+    try {
+      let user = await User.findOne({ email: email });
+
+      if (!user) {
+        return res.status(404).json({ message: "Usuário não encontrado" });
+      }
+      const isPasswordCorrect = await bcrypt.compare(password, user.password);
+
+      if (isPasswordCorrect) {
+        return res.json({ message: "Login realizado com sucesso!" });
+      } else {
+        return res.status(401).json({ message: "Senha incorreta" });
+      }
+    } catch (e) {
+      return res.status(500).json({
+        message: `Erro no processo de login: ${e.message}`,
       });
     }
   }
